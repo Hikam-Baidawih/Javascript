9295022e68c0a31dd2e75db73c44e04bcdbbc7df
Hikam-Baidawih <hikambaidawih@gmail.com>
2024-07-21 18:34:07 +0700

    Memperlajari while dan for loop

--- Javascript Dasar/loop.js
+++ Javascript Dasar/loop.js
@@ -1,6 +1,13 @@
 var tes = 1; 
+var angkotBeroperasi = 6;
+var jmlhAngkot = 10;
 
-while (tes <= 10) {
+while (tes <= angkotBeroperasi) {
   console.log("Angkot No. " + tes + " beroperasi dengan baik");
   tes++;
+}
+
+for (tes; tes <= jmlhAngkot; tes++) {
+  
+  console.log('Angkot No. ' + tes + ' tidak dapat beroperasi');
 }
